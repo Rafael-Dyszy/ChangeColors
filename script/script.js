b525096225867a2b4edd946301b6dd5769dfdce5
rafael.dyszy <rafaeldyszy@hotmail.com>
2020-07-30 18:59:36 -0400

    update comments

--- script/script.js
+++ script/script.js
@@ -6,28 +6,32 @@ function start() {
 }
 
 function changeColor() {
+  // get inputs on HTML
   let red = document.getElementById('red').value;
   let green = document.getElementById('green').value;
   let blue = document.getElementById('blue').value;
 
-  // colocando a cor no h2
+  // input colors on body
   let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
   document.getElementById('color').innerHTML = ': ' + color;
 
+  // input red on RGB box
   let colorRed = 'rgb(' + red + ',' + 0 + ',' + 0 + ')';
   document.getElementById('red').style.backgroundColor = colorRed;
 
+  // input green on RGB box
   let colorGreen = 'rgb(' + 0 + ',' + green + ',' + 0 + ')';
   document.getElementById('green').style.backgroundColor = colorGreen;
 
+  // input blue on RGB box
   let colorBlue = 'rgb(' + 0 + ',' + 0 + ',' + blue + ')';
   document.getElementById('blue').style.backgroundColor = colorBlue;
 
-  // colocar rgb na div boxcolor
+  // put rgb on div boxcolor
   document.getElementById('boxColor').style.backgroundColor = color;
 }
 
-// evento para input de cores no style
+// evetn to input  colors on style
 document.getElementById('red').addEventListener('input', changeColor);
 document.getElementById('green').addEventListener('input', changeColor);
 document.getElementById('blue').addEventListener('input', changeColor);
